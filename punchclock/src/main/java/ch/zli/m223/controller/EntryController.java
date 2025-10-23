24da9a97869fccff2a3300ac922295b1b5f9abdc
mtraub <mario.traub@ergon.ch>
2025-10-23 15:50:19 +0200

    chore: init punchclock project

---
+++ punchclock/src/main/java/ch/zli/m223/controller/EntryController.java
@@ -0,0 +1,41 @@
+package ch.zli.m223.controller;
+
+import java.util.List;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+import ch.zli.m223.model.Entry;
+import ch.zli.m223.service.EntryService;
+
+@Path("/entries")
+@Tag(name = "Entries", description = "Handling of entries")
+public class EntryController {
+
+    @Inject
+    EntryService entryService;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Index all Entries.", description = "Returns a list of all entries.")
+    public List<Entry> index() {
+        return entryService.findAll();
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Creates a new entry.", description = "Creates a new entry and returns the newly added entry.")
+    public Entry create(Entry entry) {
+       return entryService.createEntry(entry);
+    }
+
+}
---
+++ punchclock/src/main/java/ch/zli/m223/service/EntryService.java
@@ -0,0 +1,27 @@
+package ch.zli.m223.service;
+
+import java.util.List;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+
+import ch.zli.m223.model.Entry;
+
+@ApplicationScoped
+public class EntryService {
+    @Inject
+    private EntityManager entityManager;
+
+    @Transactional
+    public Entry createEntry(Entry entry) {
+        entityManager.persist(entry);
+        return entry;
+    }
+
+    public List<Entry> findAll() {
+        var query = entityManager.createQuery("FROM Entry", Entry.class);
+        return query.getResultList();
+    }
+}
